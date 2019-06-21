df6e5e07708a23ad720f339ebd44e2865fd1a53d
zartech22 <kevin.plestan@outlook.fr>
2019-06-21 09:33:03 +0200

    Add back the 'try' block in case of there is no code in body Even if it is a string and it has a ':' character

--- api/lumext_api/lumext.py
+++ api/lumext_api/lumext.py
@@ -130,10 +130,16 @@ class MessageWorker(Thread):
     def proceed_response(self, body, code: int=200):
         """Respond to the initial request
         """
-        # Parse error message to get HTTP code (ex: "404: Not found")
-        if isinstance(body, str) and len(body.split(':')) > 1:
-            code = int(body.split(':')[0].strip())
-            body = body.split(':')[1].strip()
+        try:
+            # Parse error message to get HTTP code (ex: "404: Not found")
+            if isinstance(body, str) and len(body.split(':')) > 1:
+                code = int(body.split(':')[0].strip())
+                body = body.split(':')[1].strip()
+        except Exception as e:
+            logger.critical(f"Cannot determine response code from body: {body}/{str(e)}.")
+            code = 500
+            body = "Server error in response parsing."
+        
         # Send response
         self.response_properties['statusCode'] = code
         if code >= 400: # convert str to dict
